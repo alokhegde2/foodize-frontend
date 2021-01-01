c4f4cf15db9bbf87db5aadd3e0ae7ad9eea3dd56
Alok Hegde <hegdealok@yahoo.com>
2021-01-01 22:27:54 +0530

    Commit #07

---
+++ File_basics/24:Compressing_a_file/main.go
@@ -0,0 +1,40 @@
+//Compressed is in yhe form of .tar.gz
+//This example uses gzip but standard library also
+//supports zlib,bz2,fate,and lzw
+
+package main
+
+import (
+	"compress/gzip"
+	"log"
+	"os"
+)
+
+func main() {
+	//Create .gz file to write to
+	outputFile, err := os.Create("test.txt.gz")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//Create a gzip writter on top of file writer
+
+	gzipWriter := gzip.NewWriter(outputFile)
+	defer gzipWriter.Close()
+
+	//When we write to the gzip writter
+	//it will in turn compress the contents
+	//and then write it to the underlying
+	//file writer as well
+	//we don't have to worry about how all
+	//the compression works since we just
+	//use it as a simple writer interface
+	//that we send bytes to
+
+	_, err = gzipWriter.Write([]byte("Gophers rule!\n"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Compressed data written to file.")
+}
